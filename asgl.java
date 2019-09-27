504c6b9eb4e6ca7dbe02bb2a9243bd581d66999f
James Pearson <james_pearson@outlook.com>
2019-09-27 18:58:34 +0100

    Added function to calculate mod

--- asgl.java
+++ asgl.java
@@ -1,11 +1,25 @@
 class asgl {
     public static void main (String[] args) {
-        Long a = Long.parseLong(args[0]);
-        Long j = Long.parseLong(args[1]);
+        Long a = Long.parseLong(args[0]);  //Base
+        Long j = Long.parseLong(args[1]);  //Exponent
         Long n = Long.parseLong(args[2]);
 
         System.out.println(a);
         System.out.println(j);
         System.out.println(n);
+
+        System.out.print(a.toString() + " mod " + n.toString() + " = ");
+
+        System.out.println(calculateMod(a, n));
+
+        //for (Long i = 0; i < 5; i++) {
+        //    System.out.println(i);
+        // }
+          
+    }
+
+    private static Long calculateMod(Long firstNum, Long secondNum){
+        Long result = firstNum%secondNum + (firstNum >= 0 ? 0 : secondNum);
+        return result;
     }
 }
