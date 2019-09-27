b764d32c06182b9593a96733f481e725c87b205f
James Pearson <james_pearson@outlook.com>
2019-09-27 18:42:52 +0100

    Accept command line paramaters as Long

---
+++ asgl.java
@@ -0,0 +1,11 @@
+class asgl {
+    public static void main (String[] args) {
+        Long a = Long.parseLong(args[0]);
+        Long j = Long.parseLong(args[1]);
+        Long n = Long.parseLong(args[2]);
+
+        System.out.println(a);
+        System.out.println(j);
+        System.out.println(n);
+    }
+}
