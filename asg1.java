b78108bba92e2fa71673f35a198101790d19bb4d
James Pearson <james_pearson@outlook.com>
2019-10-03 16:48:59 +0100

    Update
    Extra comments
    isEven function
    Tried to make an advancedSolve function

--- asg1.java
+++ asg1.java
@@ -6,40 +6,76 @@ public class asg1 {
         Long j = Long.parseLong(args[1]);  //Exponent
         Long n = Long.parseLong(args[2]);
 
-        //System.out.println(j/n);
-
+        //Test inputs
         /*System.out.println(a);
         System.out.println(j);
         System.out.println(n);*/
 
+        //Test calculateMod
         /*System.out.print(a.toString() + " mod " + n.toString() + " = ");
         System.out.println(calculateMod(a, n));*/
 
+        //Test isPrime
         /*System.out.println(a + "" + isPrime(a));
         System.out.println(j + "" + isPrime(j));
         System.out.println(n + "" + isPrime(n));
         System.out.println("---");*/
 
-        //if (isPrime(n)){
-        //    j = j/n;
-        //    System.out.println(j);
-        //}
+        //Test isEven
+        /*System.out.println(a + "" + isEven(a));
+        System.out.println(j + "" + isEven(j));
+        System.out.println(n + "" + isEven(n));
+        System.out.println("---");*/
 
-        Long result = 1L;  //L is needed to tell it the number is Long
+        System.out.println(simpleSolve(a, j, n));
+        //System.out.println(advancedSolve(a, j, n));
+    }
+
+    private static Long simpleSolve(Long a, Long j, Long n){
+        Long temp = 1L;  //L is needed to tell it the number is Long
         //Loop j times
         for (Long i = 0L; i < j; i++) {
-            result = result * calculateMod(a, n);
+            temp = temp * calculateMod(a, n);
             //System.out.println(i);
-            //System.out.println(result);
+            //System.out.println(temp);
         }
-
-        System.out.println(calculateMod(result, n));  //Final result
+        return calculateMod(temp, n);  //Final result
     }
+
+    // private static Long advancedSolve(Long a, Long j, Long n){
+    //     //TEST CODE - REMOVE BEFORE FINAL VERSION
+    //     if (isEven(j) != true){
+    //         System.out.println("WARNING - THIS MIGHT BREAK");
+    //     }
+
+    //     for (int i = 0; i < 50; i++) {
+    //         j = j/2;
+    //         System.out.println("i = " + i + " - j = " + j);
+    //     }
+
+    //     Long temp = 1L;
+    //     //long splitResult = simpleSolve(a, j, n);
+    //     System.out.println("splitResult = " + splitResult);
+    //     for (int i = 0; i < 50; i++) {
+    //         temp = temp * splitResult;
+    //         System.out.println("i2 = " + i + " - temp = " + temp);
+    //     }
+    //     return calculateMod(temp, n);  //Final result
+    // }
 
     //A function to calculate firstNum mod secondNum
     private static Long calculateMod(Long firstNum, Long secondNum){
         Long modResult = firstNum%secondNum + (firstNum >= 0 ? 0 : secondNum);
         return modResult;
+    }
+
+    //A function to calculate if a number is even
+    private static Boolean isEven(Long input){
+        Boolean result = false;
+        if (calculateMod(input, 2L) == 0){
+            result = true;
+        }
+        return result;
     }
 
     //A function to tell whether a number is prime (it will make an approximation for large numbers for speed)
